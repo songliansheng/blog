a7a9ed98912cf5bff4e2cb9373c267f1ad373909
songliansheng <songlianshen@outlook.com>
2023-01-12 23:42:31 +0800

    Initial commit

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
-    // webpack: (
-    //     config,
-    //     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-    // ) => {
-    //     // Important: return the modified config
-    //     return config
-    // },
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
