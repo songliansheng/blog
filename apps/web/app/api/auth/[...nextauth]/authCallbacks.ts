403d5d4a44b3f6d7416d6ed6bac266e17e59e2a0
songliansheng <songlianshen@outlook.com>
2024-03-31 10:04:10 +0800

    Initial commit

--- apps/web/app/api/auth/[...nextauth]/authCallbacks.ts
+++
@@ -1,7 +0,0 @@
-// TODO authorized isn't a callback in nextauth.js
-const callbacks = {
-    async signIn({ user, account, profile, email, credentials }) {
-        return true
-    },
-}
-export {callbacks}
