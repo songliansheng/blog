8c8954e943ff232308b0a30c15c6cb21576a76cf
songliansheng <songlianshen@outlook.com>
2024-04-06 17:52:52 +0800

    Initial commit

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,5 +1,5 @@
 //TODO: Dive deep in this
-import NextAuth from "next-auth"
+import NextAuth from 'next-auth'
 import { githubProvider, credentialsProvider } from './authProviders'
 import { signIn } from './authCallbacks'
 const handler = NextAuth({
