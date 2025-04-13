2c9db63eb43d9ff972fb7f85bbe61a939703962c
songliansheng <songlianshen@outlook.com>
2025-04-13 21:26:12 +0800

    Initial commit

--- packages/tailwind-config/tailwind-v3.config.js
+++ packages/tailwind-config/tailwind-v3.config.js
@@ -11,7 +11,7 @@ export default {
     content: [
         './app/**/*.{js,ts,jsx,tsx,mdx}',
         './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './lib/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
         extend: {
