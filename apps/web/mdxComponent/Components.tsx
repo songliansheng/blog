09d10ba4275e99b26c695278511b9491e33bf06e
songliansheng <songlianshen@outlook.com>
2025-06-14 12:35:21 +0800

    Initial commit

--- apps/web/mdxComponent/Components.tsx
+++ apps/web/mdxComponent/Components.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
 import clsx from "clsx";
 export const BR = () => <br className="" />;
-export const CODE = ({ children, ...props }) => (
+export const CODE = ({ children, ...props }:{children:any}) => (
   <>
     <code className="dark:bg-(--color-onyx)" {...props}>
       {children}
     </code>
   </>
 );
-export const H1 = ({ children }) => <h1 className="text-2xl">{children}</h1>;
+export const H1 = ({ children }: { children: any }) => (
+  <h1 className="text-2xl">{children}</h1>
+);
 /*  className="mb-8 h-[3px] bg-gray-600"  */
-export const H2 = ({ children, ...props }) => (
+export const H2 = ({ children, ...props }: { children: any }) => (
   <>
     <h2
       className={clsx(
@@ -24,7 +26,7 @@ export const H2 = ({ children, ...props }) => (
   </>
 );
 
-export const H3 = ({ children, ...props }) => (
+export const H3 = ({ children, ...props }: { children: any }) => (
   <h3
     className={clsx(
       "scroll-mt-[calc(var(--rootheader-height)+1rem)]",
@@ -35,24 +37,24 @@ export const H3 = ({ children, ...props }) => (
     {children}
   </h3>
 );
-export const HR = ({ children, ...props }) => (
+export const HR = ({ children, ...props }: { children: any }) => (
   <hr
     className="dark:border-white/10 my-12 h-[2px] dark:bg-white/10"
     {...props}
   />
 );
 
-export const P = ({ children, ...props }) => (
+export const P = ({ children, ...props }: { children: any }) => (
   <p className="mb-4 leading-10" {...props}>
     {children}
   </p>
 );
-export const Li = ({ children, ...props }) => (
+export const Li = ({ children, ...props }: { children: any }) => (
   <li className="" {...props}>
     {children}
   </li>
 ); /* "italic capitalize font-normal" */
-export const STRONG = ({ children, ...props }) => (
+export const STRONG = ({ children, ...props }: { children: any }) => (
   <strong
     className={clsx(
       "font-normal capitalize underline decoration-[1.5px] dark:decoration-white/30 decoration-gray-600"
@@ -62,7 +64,7 @@ export const STRONG = ({ children, ...props }) => (
     {children}
   </strong>
 );
-export const UL = ({ children, ...props }) => (
+export const UL = ({ children, ...props }: { children: any }) => (
   <ul {...props} className={clsx("list-disc", " pl-4")}>
     {children}
   </ul>
